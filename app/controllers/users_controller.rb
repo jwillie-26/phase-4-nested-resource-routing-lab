d14267ebdf6d9a3deeddd6663f0b3523cdc691ab
Jesse <jessewambu@zetech.ac.ke>
2023-03-16 10:49:05 +0300

    Added soln

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
+  def index
+  users = User.all
+
+  render json: users, status: :ok
+
+  end
 
   def show
-    user = User.find_by(id: params[:id])
+    if params[:item_id]
+    user = User.find(params[:item_id])
+    else
+    user = User.find(params[:id])
+    end
     render json: user, include: :items
   end
+
+ 
 
 end
